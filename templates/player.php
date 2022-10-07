0de0e8f74ef6f6e85cce4fd3839f03880b9c43f2
Daniel Figueroa <danielf@senalcorporativo.com>
2022-10-07 16:16:24 -0500

    debugging shaka

--- templates/player.php
+++ templates/player.php
@@ -11,82 +11,93 @@ script("dashvideoplayer", "shaka-player.ui");
 </div>
 
 <script type="text/javascript" nonce="<?php p(base64_encode($_["requesttoken"])) ?>" defer>
-    window.addEventListener('DOMContentLoaded', function() {    
-        function initApp() {
-           console.log("DASH PLAYER WILL START");
+    window.addEventListener('DOMContentLoaded', function() {
+
+        const manifestUri = '<?php p($videoUrl) ?>'
+        async function init() {
+            // Install built-in polyfills to patch browser incompatibilities.
+            shaka.polyfill.installAll();
+
+            // Check to see if the browser supports the basic APIs Shaka needs.
+            if (shaka.Player.isBrowserSupported()) {
+                // Everything looks good!
+                console.log("SHAKA WILL START NOW");
+                initPlayer();
+            } else {
+                // This browser does not have the minimum set of APIs we need.
+                console.error('Browser not supported!');
+            }
+            return;
+            
+            const video = document.getElementById('video');
+            const ui = video['ui'];
+            const config = {
+                'controlPanelElements': [
+                    'time_and_duration',
+                    'play_pause',
+                    'mute',
+                    'volume',
+                    'airplay',
+                    'cast',
+                    'fullscreen',
+                    'captions',
+                    'quality'
+                ],
+            };
+            ui.configure(config);
+
+            const controls = ui.getControls();
+            const player = controls.getPlayer();
+
+            // Attach player and ui to the window to make it easy to access in the JS console.
+            window.player = player;
+            window.ui = ui;
+
+            // Listen for error events.
+            player.addEventListener('error', onPlayerErrorEvent);
+            controls.addEventListener('error', onUIErrorEvent);
+
+            // Try to load a manifest.        
+            try {
+                await player.load(manifestUri);
+                player.addTextTrackAsync(<?php p($subtitlesUrl) ?>, 'fr-CA', 'subtitles');
+                player.setTextTrackVisibility(true);
+                await player.configure({
+                    preferredTextLanguage: 'fr-CA',
+                });
+                console.log(">>>>>>>>>>> SHAKA STARTED");
+            } catch (error) {
+                console.log(">>>>>>>>>>> SHAKA ERROR: ", error);
+                onPlayerError(error);
+            }
         }
-        initApp();
+
+        function onPlayerErrorEvent(errorEvent) {
+            // Extract the shaka.util.Error object from the event.
+            onPlayerError(event.detail);
+        }
+
+        function onPlayerError(error) {
+            // Handle player error
+            console.error('Error code', error.code, 'object', error);
+        }
+
+        function onUIErrorEvent(errorEvent) {
+            // Extract the shaka.util.Error object from the event.
+            onPlayerError(event.detail);
+        }
+
+        function initFailed(errorEvent) {
+            // Handle the failure to load; errorEvent.detail.reasonCode has a
+            // shaka.ui.FailReasonCode describing why.
+            console.error('Unable to load the UI library!');
+        }
+
+        // Listen to the custom shaka-ui-loaded event, to wait until the UI is loaded.
+        document.addEventListener('shaka-ui-loaded', init);
+        // Listen to the custom shaka-ui-load-failed event, in case Shaka Player fails
+        // to load (e.g. due to lack of browser support).
+        document.addEventListener('shaka-ui-load-failed', initFailed);
+
     });
-
-    /*const manifestUri = '<?php p($videoUrl) ?>'
-    async function init() {    
-        const video = document.getElementById('video');
-        const ui = video['ui'];
-        const config = {
-            'controlPanelElements': [
-                'time_and_duration',
-                'play_pause',
-                'mute',
-                'volume',
-                'airplay',
-                'cast',
-                'fullscreen',
-                'captions',
-                'quality'
-            ],
-        };
-        ui.configure(config);
-
-        const controls = ui.getControls();
-        const player = controls.getPlayer();
-
-        // Attach player and ui to the window to make it easy to access in the JS console.
-        window.player = player;
-        window.ui = ui;
-
-        // Listen for error events.
-        player.addEventListener('error', onPlayerErrorEvent);
-        controls.addEventListener('error', onUIErrorEvent);
-
-        // Try to load a manifest.        
-        try {
-            await player.load(manifestUri);
-            player.addTextTrackAsync(<?php p($subtitlesUrl) ?>, 'fr-CA', 'subtitles');
-            player.setTextTrackVisibility(true);
-            await player.configure({
-                preferredTextLanguage: 'fr-CA',
-            });
-            console.log(">>>>>>>>>>> SHAKA STARTED");
-        } catch (error) {
-            console.log(">>>>>>>>>>> SHAKA ERROR: ", error);
-            onPlayerError(error);
-        }
-    }
-
-    function onPlayerErrorEvent(errorEvent) {
-        // Extract the shaka.util.Error object from the event.
-        onPlayerError(event.detail);
-    }
-
-    function onPlayerError(error) {
-        // Handle player error
-        console.error('Error code', error.code, 'object', error);
-    }
-
-    function onUIErrorEvent(errorEvent) {
-        // Extract the shaka.util.Error object from the event.
-        onPlayerError(event.detail);
-    }
-
-    function initFailed(errorEvent) {
-        // Handle the failure to load; errorEvent.detail.reasonCode has a
-        // shaka.ui.FailReasonCode describing why.
-        console.error('Unable to load the UI library!');
-    }
-
-    // Listen to the custom shaka-ui-loaded event, to wait until the UI is loaded.
-    document.addEventListener('shaka-ui-loaded', init);
-    // Listen to the custom shaka-ui-load-failed event, in case Shaka Player fails
-    // to load (e.g. due to lack of browser support).
-    document.addEventListener('shaka-ui-load-failed', initFailed);*/
 </script>
